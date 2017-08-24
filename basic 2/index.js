ec9b020af54cab8247d10e3937ae67235421087e
alid02 <aliddidarali02@gmail.com>
2017-08-24 09:15:01 +0100

    Write a JavaScript program to print the contents of the current window.

---
+++ basic 2/index.js
@@ -0,0 +1,11 @@
+var button = document.getElementById("btn print");
+
+// button.addEventListener('click',my_window);
+
+
+
+addEventListener("click",my_window);
+
+function my_window() {
+    window.print();
+};
